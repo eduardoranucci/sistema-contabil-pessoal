1ac2a899996685790b58ae0b34052e4b149281ea
Eduardo Ranucci <81530198+eduardoranucci@users.noreply.github.com>
2022-04-24 21:36:28 -0300

    edição e exclusão

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from prettytable import PrettyTable
 
 class Conta:
 
-    def __init__(self, nome, classificacao):
+    def __init__(self, nome, classificacao, adicionar_na_base=False):
         
         self.nome = nome
         self.classificacao = classificacao
@@ -23,6 +23,9 @@ class Conta:
             except ValueError:
                 print('Erro: Classificação já existente.', end='\n\n')
                 self.classificacao = int(input('Classificação: '))
+
+        if adicionar_na_base:
+            base_contas.append([self.cod, self.nome, self.classificacao])
 
     def verifica_classificacao(self):
         
@@ -60,23 +63,37 @@ class Conta:
 
         print(f'\n{tabela}')
 
+    def edita_conta(cod):
+        
+        for cell in base_contas['A']:
+            if cell.value == cod:
+                linha = cell.row
+                print()
+                nome = input('Nome: ')
+                classificacao = int(input('Classificação: '))
+                Conta(nome, classificacao)
+
+                base_contas[f'B{linha}'] = nome
+                base_contas[f'C{linha}'] = classificacao
+
+    def deleta_conta(cod):
+
+        for cell in base_contas['A']:
+            if cell.value == cod:
+                base_contas.delete_rows(cell.row)
+
 
 class Lancamento:
 
     global base_lancamentos
 
-    def __init__(self, valor, data, debito, credito, descricao, parcelas):
+    def __init__(self, valor, data, debito, credito, descricao, adicionar_na_base=False):
         
         self.valor = valor
         self.data = data
         self.debito = debito
         self.credito = credito
         self.descricao = descricao
-        
-        if parcelas == '':
-            self.parcelas = 1
-        else:
-            self.parcelas = int(parcelas)
         
         try:
             total_lancamentos = len(base_lancamentos['A'])
@@ -90,7 +107,7 @@ class Lancamento:
                 self.verifica_debito()
                 debito_valido = True
             except ValueError:
-                print('Erro: Conta não existente.', end='\n\n')
+                print('Erro: Conta débito não existente.', end='\n\n')
                 self.debito = int(input('Débito: '))
 
         credito_valido = False
@@ -99,8 +116,11 @@ class Lancamento:
                 self.verifica_credito()
                 credito_valido = True
             except ValueError:
-                print('Erro: Conta não existente.', end='\n\n')
+                print('Erro: Conta crédito não existente.', end='\n\n')
                 self.credito = int(input('Crédito: '))
+
+        if adicionar_na_base:
+            base_lancamentos.append([self.cod, self.data, self.debito, self.credito, self.valor, self.descricao])
 
     def verifica_debito(self):
         
@@ -131,7 +151,7 @@ class Lancamento:
         coluna_a, coluna_b, coluna_c = [], [], []
         coluna_d, coluna_e, coluna_f = [], [] ,[]
             
-        for column in base_contas['A:F']:
+        for column in base_lancamentos['A:F']:
             
             for cell in column:
                 if cell.row == 1:
@@ -143,7 +163,7 @@ class Lancamento:
                         coluna_a.append(cell.value)
 
                     case 2:
-                        coluna_b.append(cell.value)
+                        coluna_b.append(str(cell.value))
 
                     case 3:
                         coluna_c.append(cell.value)
@@ -166,6 +186,31 @@ class Lancamento:
 
         print(f'\n{tabela}')
 
+    def edita_lancamento(cod):
+        
+        for cell in base_contas['A']:
+            if cell.value == cod:
+                linha = cell.row
+                print()
+                data = input('Data: ')
+                debito = int(input('Débito: '))
+                credito = int(input('Crédito: '))
+                valor = float(input('Valor: '))
+                descricao = input('Descrição: ')
+                Lancamento(valor, data, debito, credito, descricao)
+
+                base_lancamentos[f'B{linha}'] = data
+                base_lancamentos[f'C{linha}'] = debito
+                base_lancamentos[f'D{linha}'] = credito
+                base_lancamentos[f'E{linha}'] = valor
+                base_lancamentos[f'F{linha}'] = descricao
+
+    def deleta_lancamento(cod):
+
+        for cell in base_lancamentos['A']:
+            if cell.value == cod:
+                base_lancamentos.delete_rows(cell.row)
+
 try:
     df = xl.load_workbook(r'base.xlsx')
 
@@ -187,10 +232,10 @@ except FileNotFoundError:
 
     df.save('base.xlsx')
 
-print('\nSistema contabil', end='\n\n')
+print('\nSistema contábil', end='\n\n')
 print('1 - Criar contas\n2 - Lançar\n3 - Consultar plano de contas')
-print('4 - Consultar lançamentos\n5 - Editar conta\n6 - Editar lançamento\n')
-
+print('4 - Consultar lançamentos\n5 - Editar conta\n6 - Editar lançamento')
+print('7 - Deletar conta\n8 - Deletar lançamento\n')
 
 escolha = int(input('O que você deseja fazer? '))
 
@@ -200,9 +245,7 @@ match escolha:
         print()
         nome_conta = input('Nome da conta: ')
         class_conta = int(input('Classificação: '))
-        conta = Conta(nome_conta, class_conta)
-
-        base_contas.append([conta.cod, conta.nome, conta.classificacao])
+        conta = Conta(nome_conta, class_conta, True)
 
     case 2:
         print()
@@ -211,10 +254,8 @@ match escolha:
         credito_lanc = int(input('Crédito: '))
         valor_lanc = float(input('Valor: '))
         descr_lanc = input('Descrição: ')
-        parcelas_lanc = input('Parcelas: ')
-        lanc = Lancamento(valor_lanc, data_lanc, debito_lanc, credito_lanc, descr_lanc, parcelas_lanc)
-
-        base_lancamentos.append([lanc.cod, lanc.data, lanc.debito, lanc.credito, lanc.valor, lanc.descricao])
+        parcelas = input('Parcelas: ')
+        lanc = Lancamento(valor_lanc, data_lanc, debito_lanc, credito_lanc, descr_lanc, True)
 
     case 3:
         Conta.consulta_plano_de_contas()
@@ -223,12 +264,22 @@ match escolha:
         Lancamento.consulta_lancamentos()
 
     case 5:
-        pass
+        print()
+        Conta.edita_conta(int(input('Código da conta: ')))
 
     case 6:
-        pass
+        print()
+        Lancamento.edita_lancamento(int(input('Código do lançamento: ')))
+
+    case 7:
+        print()
+        Conta.deleta_conta(int(input('Código da conta: ')))
+
+    case 8:
+        print()
+        Lancamento.deleta_lancamento(int(input('Código do lançamento: ')))
 
     case opcao_invalida:
-        print('Opção invalida.')
+        print(f'{opcao_invalida} não é uma opção valida.')
 
 df.save('base.xlsx')
