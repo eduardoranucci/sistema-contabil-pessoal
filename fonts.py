27c76fbd200a1a71f3daecfdfb3018f7e9336458
Eduardo Ranucci <81530198+eduardoranucci@users.noreply.github.com>
2022-07-12 17:28:00 -0300

    Add files via upload

--- fonts.py
+++ fonts.py
@@ -31,24 +31,59 @@
 
 # populate(frame)
 
+# # root.mainloop()
+
+# import tkinter as tk
+# from tkinter import ttk
+
+# class App:
+#     def __init__(self):
+#         self.root = tk.Tk()
+#         self.tree = ttk.Treeview()
+#         self.tree.pack()
+#         for i in range(10):
+#             self.tree.insert("", "end", text="Item %s" % i)
+#         self.tree.bind("<Double-1>", self.OnDoubleClick)
+#         self.root.mainloop()
+
+#     def OnDoubleClick(self, event):
+#         item = self.tree.selection()[0]
+#         print("you clicked on", self.tree.item(item,"text"))
+
+# if __name__ == "__main__":
+#     app = App()
+    
+    
+# # Import Required Library
+# from tkinter import *
+# #import win32api
+# from tkinter import filedialog
+  
+# # Create Tkinter Object
+# root = Tk()
+  
+# # Set Title and geometry
+# root.title('Print Hard Copies')
+# root.geometry("200x200")
+ 
+# def file_save():
+#     f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+#     if f is None: # asksaveasfile return `None` if dialog closed with "cancel".
+#         return
+#     text2save = str('Teste') # starts from `1.0`, not `0.0`
+#     f.write(text2save)
+#     f.close() # `()` was missing. 
+  
+# # Make Button
+# Button(root, text="Print File", command=file_save).pack(pady=75)
+  
+# # Execute Tkinter
 # root.mainloop()
 
-import tkinter as tk
-from tkinter import ttk
+clas = '1234567890'
+meta = '1.2.34.56.7890'
+print(len(clas))
 
-class App:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.tree = ttk.Treeview()
-        self.tree.pack()
-        for i in range(10):
-            self.tree.insert("", "end", text="Item %s" % i)
-        self.tree.bind("<Double-1>", self.OnDoubleClick)
-        self.root.mainloop()
-
-    def OnDoubleClick(self, event):
-        item = self.tree.selection()[0]
-        print("you clicked on", self.tree.item(item,"text"))
-
-if __name__ == "__main__":
-    app = App()
+clas_p = f'{clas[0]}.{clas[1]}.{clas[2:4]}.{clas[4:6]}.{clas[6:]}'
+print(meta)
+print(clas_p)
--- tk.py
+++ tk.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 from tkcalendar import DateEntry
-from prettytable import PrettyTable
+from datetime import date
 
 
 def atualizar_tree_contas():
@@ -44,11 +44,15 @@ def editar_conta():
 
 
 def deletar_conta():
-    if valor_codigo.get() == '':
-        messagebox.showinfo(title='Erro', message='Digite o código da conta.')
+    selecao = tree_contas.focus()
+    valores = tree_contas.item(selecao, 'values')
+    
+    if valores == '':
+        messagebox.showinfo(title='Erro', message='Selecione uma conta.')
         return
     
-    Conta.deleta_conta(int(valor_codigo.get()))
+    print(valores[2])
+    Conta.deleta_conta(int(valores[2]))
 
 
 def duplo_clique_contas(event):
@@ -90,13 +94,34 @@ def lancar():
         return
     
     Lancamento(valor_valor.get(), valor_data.get(), valor_debito.get(), valor_credito.get(), valor_descricao_lanc.get('1.0', 'end-1c'), True)
+    #limpar_campos_lancamentos()
 
 
 def editar_lancamento():
-    pass
+    if valor_valor.get() == '' or valor_data.get() == '' or valor_debito.get() == '' or valor_credito.get() == '' or valor_descricao_lanc.get('1.0', 'end-1c') == '':
+        messagebox.showinfo(title='Erro', message='Preencha todos os campos.')
+        print('erro')
+        return
+
+    if valor_debito.get() == valor_credito.get():
+        messagebox.showinfo(title='Erro', message='Conta débito não pode ser igual a conta crédito.')
+        return
+    
+    Lancamento.edita_lancamento(int(valor_id))
+    limpar_campos_lancamentos()
+
 
 def deletar_lancamento():
-    pass
+
+    selecao = tree_lancamentos.focus()
+    valores = tree_lancamentos.item(selecao, 'values')
+    
+    if valores == '':
+        messagebox.showinfo(title='Erro', message='Selecione um lançamento.')
+        return
+    
+    Lancamento.deleta_lancamento(int(valores[0]))
+    limpar_campos_lancamentos()
 
 def duplo_clique_lancamentos(event):
     limpar_campos_lancamentos()
@@ -104,11 +129,14 @@ def duplo_clique_lancamentos(event):
     selecao = tree_lancamentos.focus()
     valores = tree_lancamentos.item(selecao, 'values')
     
+    global valor_id
+    valor_id = valores[0]
     valor_data.insert(0, valores[1])
     valor_debito.insert(0, valores[2])
-    valor_credito.insert(0, valores[2])
+    valor_credito.insert(0, valores[3])
     valor_valor.insert(0, valores[4])
     valor_descricao_lanc.insert('1.0', valores[5])
+    
 
 class Conta:
 
@@ -123,17 +151,15 @@ class Conta:
                 if cell.value == self.codigo:
                     raise ValueError
         except ValueError:
-            messagebox.showinfo(title='Erro!', message='Código já existente.')
             adicionar_na_base = False
 
         try:
             self.verifica_classificacao()
         except ValueError:
-            messagebox.showinfo(title='Erro!', message='Classificação já existente.')
             adicionar_na_base = False
 
         if adicionar_na_base:
-            base_contas.append([self.codigo, self.nome, self.classificacao])
+            base_contas.append([int(self.codigo), self.nome, self.classificacao])
             df.save('base.xlsx')
             tree_contas.insert('', END, values=(self.classificacao, self.nome, self.codigo))
             limpar_campos_conta()
@@ -143,6 +169,12 @@ class Conta:
         
         for cell in base_contas['C']:
             if cell.value == self.classificacao:
+                messagebox.showinfo(title='Erro!', message='Código já existente.')
+                raise ValueError
+            
+            validos = [1, 2, 4, 6, 10]
+            if len(self.classificacao) not in validos:
+                messagebox.showinfo(title='Erro!', message='Classificação inválida.')
                 raise ValueError
 
     def edita_conta(cod):
@@ -152,7 +184,7 @@ class Conta:
                 linha = cell.row
                 nome = valor_descricao_conta.get()
                 classificacao = valor_classificacao.get()
-                cod = valor_codigo.get()
+                cod = int(valor_codigo.get())
                 #Conta(nome, classificacao, cod)
 
                 base_contas[f'B{linha}'] = nome
@@ -163,14 +195,14 @@ class Conta:
                 messagebox.showinfo(title='Sucesso!', message='Conta alterada com sucesso.')
 
     def deleta_conta(cod):
-
+        
         for cell in base_contas['A']:
-            if cell.value == valor_codigo.get():
+            if cell.value == cod:
                 base_contas.delete_rows(cell.row)
                 df.save('base.xlsx')
                 atualizar_tree_contas()
                 limpar_campos_conta()
-                messagebox.showinfo(title='Sucesso!', message='Conta deletada com sucesso.')
+                #messagebox.showinfo(title='Sucesso!', message='Conta deletada com sucesso.')
 
 
 class Lancamento:
@@ -202,12 +234,19 @@ class Lancamento:
         except ValueError:
             messagebox.showinfo(title='Erro', message='Conta crédito inexistente.')
             adicionar_na_base = False
-
+            
+        try:    
+            if self.credito == self.debito:
+                raise ValueError
+        except:
+            messagebox.showinfo(title='Erro', message='Conta débito não pode ser igual a conta crédito.')
+            adicionar_na_base = False
+        
         if adicionar_na_base:
             base_lancamentos.append([self.cod, self.data, self.debito, self.credito, self.valor, self.descricao])
             df.save('base.xlsx')
             atualizar_tree_lancamentos()
-            limpar_campos_conta()
+            limpar_campos_lancamentos()
             messagebox.showinfo(title='Sucesso!', message='Lançamento efetuado com sucesso.')
 
     def verifica_debito(self):
@@ -232,59 +271,16 @@ class Lancamento:
         else:
             raise ValueError
 
-    def consulta_lancamentos():
-        
-        tabela = PrettyTable()
-
-        coluna_a, coluna_b, coluna_c = [], [], []
-        coluna_d, coluna_e, coluna_f = [], [] ,[]
-            
-        for column in base_lancamentos['A:F']:
-            
-            for cell in column:
-                if cell.row == 1:
-                    continue
-
-                match cell.column:
-
-                    case 1:
-                        coluna_a.append(cell.value)
-
-                    case 2:
-                        coluna_b.append(str(cell.value))
-
-                    case 3:
-                        coluna_c.append(cell.value)
-
-                    case 4:
-                        coluna_d.append(cell.value)
-
-                    case 5:
-                        coluna_e.append(cell.value)
-
-                    case 6:
-                        coluna_f.append(cell.value)
-            
-        tabela.add_column('Código', coluna_a)
-        tabela.add_column('Data', coluna_b)
-        tabela.add_column('Débito', coluna_c)
-        tabela.add_column('Crédito', coluna_d)
-        tabela.add_column('Valor', coluna_e)
-        tabela.add_column('Descrição', coluna_f)
-
-        print(f'\n{tabela}')
-
     def edita_lancamento(cod):
         
-        for cell in base_contas['A']:
+        for cell in base_lancamentos['A']:
             if cell.value == cod:
                 linha = cell.row
-                print()
-                data = input('Data: ')
-                debito = int(input('Débito: '))
-                credito = int(input('Crédito: '))
-                valor = float(input('Valor: '))
-                descricao = input('Descrição: ')
+                data = valor_data.get()
+                debito = valor_debito.get()
+                credito = valor_credito.get()
+                valor = valor_valor.get()
+                descricao = valor_descricao_lanc.get('1.0', 'end-1c')
                 Lancamento(valor, data, debito, credito, descricao)
 
                 base_lancamentos[f'B{linha}'] = data
@@ -292,12 +288,23 @@ class Lancamento:
                 base_lancamentos[f'D{linha}'] = credito
                 base_lancamentos[f'E{linha}'] = valor
                 base_lancamentos[f'F{linha}'] = descricao
+                
+                df.save('base.xlsx')
+                atualizar_tree_lancamentos()
+                limpar_campos_lancamentos()
+                messagebox.showinfo(title='Sucesso!', message='Lançamento alterado com sucesso.')
 
     def deleta_lancamento(cod):
 
         for cell in base_lancamentos['A']:
             if cell.value == cod:
                 base_lancamentos.delete_rows(cell.row)
+                
+                df.save('base.xlsx')
+                atualizar_tree_lancamentos()
+                limpar_campos_lancamentos()
+                #messagebox.showinfo(title='Sucesso!', message='Lançamento deletado com sucesso.')
+
 
 try:
     df = xl.load_workbook(r'base.xlsx')
@@ -320,51 +327,27 @@ except FileNotFoundError:
 
     df.save('base.xlsx')
 
-# print('\nSistema contábil', end='\n\n')
-# print('1 - Criar contas\n2 - Lançar\n3 - Consultar plano de contas')
-# print('4 - Consultar lançamentos\n5 - Editar conta\n6 - Editar lançamento')
-# print('7 - Deletar conta\n8 - Deletar lançamento\n')
-
-# escolha = int(input('O que você deseja fazer? '))
-
-# match escolha:
-
-#     case 2:
-#         print()
-#         data_lanc = input('Data: ')
-#         debito_lanc = int(input('Débito: '))
-#         credito_lanc = int(input('Crédito: '))
-#         valor_lanc = float(input('Valor: '))
-#         descr_lanc = input('Descrição: ')
-#         parcelas = input('Parcelas: ')
-#         lanc = Lancamento(valor_lanc, data_lanc, debito_lanc, credito_lanc, descr_lanc, True)
-
-#     case 4:
-#         Lancamento.consulta_lancamentos()
-
-#     case 6:
-#         print()
-#         Lancamento.edita_lancamento(int(input('Código do lançamento: ')))
-
-#     case 8:
-#         print()
-#         Lancamento.deleta_lancamento(int(input('Código do lançamento: ')))
-
-########################################################################################################################################################################################
 
 main = Tk()
 main.title('Contabilidade')
 main.geometry('1000x625')
 main.resizable(False, False)
 
-# barra_menus = Menu(main)
-# menu_opcoes = Menu(barra_menus, tearoff=0)
-# menu_opcoes.add_command(label='Teste', command=main.quit)
-# menu_opcoes.add_separator()
-# menu_opcoes.add_command(label='Fechar', command=main.quit)
-# barra_menus.add_cascade(label='Opções', menu=menu_opcoes)
+barra_menus = Menu(main)
+menu_arquivo = Menu(barra_menus, tearoff=0)
+menu_arquivo.add_command(label='Deletar banco de dados')
+menu_arquivo.add_separator()
+menu_arquivo.add_command(label='Sair', command=main.quit)
 
-# main.config(menu=barra_menus)
+menu_ajuda = Menu(barra_menus, tearoff=0)
+menu_ajuda.add_command(label='Como usar')
+menu_ajuda.add_command(label='Sobre')
+menu_ajuda.add_command(label='Encontrou um erro?')
+
+barra_menus.add_cascade(label='Arquivo', menu=menu_arquivo)
+barra_menus.add_cascade(label='Ajuda', menu=menu_ajuda)
+
+main.config(menu=barra_menus)
 
 notebook = ttk.Notebook(main)
 notebook.place(x=1, y=0, width=1000, height=625)
@@ -475,12 +458,9 @@ Button(aba_lacamentos, text='Alterar', command=editar_lancamento).place(x=143, y
 
 Button(aba_lacamentos, text='Excluir', command=deletar_lancamento).place(x=264, y=350, width=75, height=25)
 
-# def valor():
-#     print(valor_descrisao_lanc.get('1.0', 'end-1c'))
-
-tree_lancamentos.insert('', END, values=('1', '14/09/2022', '1125', '1975', '10000,00', 'Saldo anterior - 31/12/2021'))
-
 # ABA RELATORIOS #
+
+ano = date.today().year
 
 Frame(aba_relatorios, bg='#000000').place(x=5, y=5, width=325, height=590)
 Frame(aba_relatorios, bg='#D4D4D4').place(x=6, y=6, width=323, height=588)
@@ -504,6 +484,11 @@ valor_data_inicial_razao.place(x=26, y=160, width=98, height=25)
 
 valor_data_final_razao = DateEntry(aba_relatorios)
 valor_data_final_razao.place(x=199, y=160, width=98, height=25)
+
+valor_data_inicial_razao.delete(0, END)
+valor_data_final_razao.delete(0, END)
+valor_data_inicial_razao.insert(0, f'01/01/{ano}')
+valor_data_final_razao.insert(0, f'31/12/{ano}')
 
 Button(aba_relatorios, text='Gerar').place(x=124, y=235, width=75, height=25)
 
@@ -536,22 +521,25 @@ valor_conta_resumo.place(x=460.5, y=80, height=25, width=75)
 
 valor_ano_resumo = Entry(aba_relatorios)
 valor_ano_resumo.place(x=460.5, y=125, height=25, width=75)
+valor_ano_resumo.insert(0, "2022")
 
 Button(aba_relatorios, text='Gerar').place(x=460.5, y=200, width=75, height=25)
 
 # balancete
 Label(aba_relatorios, text='Balancete', bg='#C0C0C0', font=('Arial', 14, 'bold')).place(x=668, y=10, width=323, height=25)
-Label(aba_relatorios, text='Conta:', font=('Arial', 12), bg='#D4D4D4').place(x=699, y=80, height=25)
-valor_conta_balancete = Entry(aba_relatorios)
-valor_conta_balancete.place(x=797, y=80, height=25, width=75)
 
-Label(aba_relatorios, text='Período:', font=('Arial', 12), bg='#D4D4D4').place(x=699, y=125, height=25)
-Label(aba_relatorios, text='a', font=('Arial', 12), bg='#D4D4D4').place(x=829.5, y=160, height=25, width=10)
+Label(aba_relatorios, text='Período:', font=('Arial', 12), bg='#D4D4D4').place(x=699, y=80, height=25)
+Label(aba_relatorios, text='a', font=('Arial', 12), bg='#D4D4D4').place(x=829.5, y=115, height=25, width=10)
 valor_data_inicial_balancete = DateEntry(aba_relatorios)
-valor_data_inicial_balancete.place(x=699, y=160, width=98, height=25)
+valor_data_inicial_balancete.place(x=699, y=115, width=98, height=25)
 
 valor_data_final_balancete = DateEntry(aba_relatorios)
-valor_data_final_balancete.place(x=872, y=160, width=98, height=25)
+valor_data_final_balancete.place(x=872, y=115, width=98, height=25)
+
+valor_data_inicial_balancete.delete(0, END)
+valor_data_final_balancete.delete(0, END)
+valor_data_inicial_balancete.insert(0, f'01/01/{ano}')
+valor_data_final_balancete.insert(0, f'31/12/{ano}')
 
 Button(aba_relatorios, text='Gerar').place(x=797, y=235, width=75, height=25)
 
